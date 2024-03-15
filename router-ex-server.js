a91859cf478fc4ebeabd2c54ed89c6b9a2229ed8
Pawan Kumar <naveen.p@northeastern.edu>
2024-03-15 21:56:29 +0530

    Class Activity (Mar 12)

--- router-ex-server.js
+++ router-ex-server.js
@@ -5,6 +5,7 @@ const birds = require('./birds');
 
 app.use('/birds', birds);
 
+//looks for url to end in /birds
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
